b1e3f47d4560d004302c6b45f823bea8e916177a
DvTonder <david.vantonder@gmail.com>
2013-02-18 09:25:26 -0500

    Settings: Add Advanced reboot (1 of 2)
    This commit adds a setting in Development settings for including options
    in the power menu for rebooting into recovery or bootloader, defauled to
    off.
    
    When enabled, the Advanced reboot options will only be available once the
    device is unlocked.
    
    Change-Id: Ib77f0be178d15131b99adbe0abb6ba18b2b7c0b6
    
    From: jackmu95 <jacob.mueller.elz@gmail.com>
    Settings: Move 'Advanced reboot' to other position
    
    This commit moves the 'Advanced reboot' checkbox one item down
    because it looks a bit strange as first element in the list IMHO.
    
    Before: http://goo.gl/jhw8h
    After:  http://goo.gl/X3H68
    
    Change-Id: I26d4a5caba3e720b952ec405e200c1528f7a7321
    
    From: ThiagoVinicius <thiagovfar@gmail.com>
    Development Settings: fix setting reset on disable
    
    When Development Settings are disabled, some settings are
    reset to values other than the default. This fixes it.
    
     - No need to have resetAdvancedReboot(), as it is already
       reset by the "unckeck all checkboxes" logic.
    
    Change-Id: If5d88d220d2a17d6e172b2f54facd4afa3bae904
    
    Conflicts:
    	res/values/cm_strings.xml
    	src/com/android/settings/DevelopmentSettings.java
    
    Conflicts:
    	res/values/strings.xml

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -174,6 +174,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
 
     private static String DEFAULT_LOG_RING_BUFFER_SIZE_IN_BYTES = "262144"; // 256K
 
+    private static final String ADVANCED_REBOOT_KEY = "advanced_reboot";
+
     private IWindowManager mWindowManager;
     private IBackupManager mBackupManager;
     private DevicePolicyManager mDpm;
@@ -240,6 +242,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
 
     private CheckBoxPreference mShowAllANRs;
     private CheckBoxPreference mKillAppLongpressBack;
+
+    private CheckBoxPreference mAdvancedReboot;
 
     private PreferenceScreen mProcessStats;
     private final ArrayList<Preference> mAllPrefs = new ArrayList<Preference>();
@@ -309,6 +313,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         mDebugViewAttributes = findAndInitSwitchPref(DEBUG_VIEW_ATTRIBUTES);
         mPassword = (PreferenceScreen) findPreference(LOCAL_BACKUP_PASSWORD);
         mAllPrefs.add(mPassword);
+        mAdvancedReboot = findAndInitCheckboxPref(ADVANCED_REBOOT_KEY);
 
 
         if (!android.os.Process.myUserHandle().equals(UserHandle.OWNER)) {
@@ -316,6 +321,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
             disableForUser(mClearAdbKeys);
             disableForUser(mEnableTerminal);
             disableForUser(mPassword);
+            disableForUser(mAdvancedReboot);
         }
 
         mDebugAppPref = findPreference(DEBUG_APP_KEY);
@@ -567,6 +573,18 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         updateSimulateColorSpace();
         updateUseNuplayerOptions();
         updateUSBAudioOptions();
+        updateAdvancedRebootOptions();
+    }
+
+    private void writeAdvancedRebootOptions() {
+        Settings.Secure.putInt(getActivity().getContentResolver(),
+                Settings.Secure.ADVANCED_REBOOT,
+                mAdvancedReboot.isChecked() ? 1 : 0);
+    }
+
+    private void updateAdvancedRebootOptions() {
+        mAdvancedReboot.setChecked(Settings.Secure.getInt(getActivity().getContentResolver(),
+                Settings.Secure.ADVANCED_REBOOT, 0) != 0);
     }
 
     private void resetDangerousOptions() {
@@ -1492,6 +1510,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
             writeUSBAudioOptions();
         } else if (preference == mKillAppLongpressBack) {
             writeKillAppLongpressBackOptions();
+        } else if (preference == mAdvancedReboot) {
+            writeAdvancedRebootOptions();
         } else {
             return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
