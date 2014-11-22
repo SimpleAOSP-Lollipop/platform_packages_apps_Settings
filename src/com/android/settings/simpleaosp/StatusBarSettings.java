625e12f650f790a9a98356190b49a8d26e4f79e9
snak3ater <saeedyasir91@gmail.com>
2014-11-22 17:31:02 +0500

    Status bar brightness control
    Conflicts:
    	src/com/android/settings/simpleaosp/StatusBarSettings.java

--- src/com/android/settings/simpleaosp/StatusBarSettings.java
+++ src/com/android/settings/simpleaosp/StatusBarSettings.java
@@ -3,6 +3,7 @@ package com.android.settings.simpleaosp;
 
 import android.content.ContentResolver;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
@@ -25,6 +26,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     private static final String STATUS_BAR_NATIVE_BATTERY_PERCENTAGE = "status_bar_native_battery_percentage";
     // Double-tap to sleep
     private static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+    private static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
     // General
     private PreferenceCategory mStatusBarGeneralCategory;
@@ -32,6 +34,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     private SwitchPreference mStatusBarNativeBatteryPercentage;
     // Double-tap to sleep
     private CheckBoxPreference mStatusBarDoubleTapSleepGesture;
+    private CheckBoxPreference mStatusBarBrightnessControl;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,6 +45,24 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
 
         // General category
         mStatusBarGeneralCategory = (PreferenceCategory) findPreference(STATUS_BAR_GENERAL_CATEGORY);
+
+ 	// Status bar brightness control
+        mStatusBarBrightnessControl = (CheckBoxPreference) getPreferenceScreen().findPreference(STATUS_BAR_BRIGHTNESS_CONTROL);
+        mStatusBarBrightnessControl.setChecked((Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL, 0) == 1));
+        try {
+            if (Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS_MODE) == Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC) {
+                mStatusBarBrightnessControl.setEnabled(false);
+                mStatusBarBrightnessControl.setSummary(R.string.status_bar_toggle_info);
+            }
+        } catch (SettingNotFoundException e) {
+        }
+
+        // don't show status bar brightnees control on tablet
+        if (Utils.isTablet(getActivity())) {
+            getPreferenceScreen().removePreference(mStatusBarBrightnessControl);
+        }
 
        // Status bar double-tap to sleep
 	mStatusBarDoubleTapSleepGesture = (CheckBoxPreference) getPreferenceScreen().findPreference(DOUBLE_TAP_SLEEP_GESTURE);
@@ -69,11 +90,17 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-	if (preference == mStatusBarDoubleTapSleepGesture) {
+	boolean value;
+        if (preference == mStatusBarBrightnessControl) {
+            value = mStatusBarBrightnessControl.isChecked();
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL, value ? 1 : 0);
+            return true;
+        } else if (preference == mStatusBarDoubleTapSleepGesture) {
 		value = mStatusBarDoubleTapSleepGesture.isChecked();
 		Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
 			Settings.System.DOUBLE_TAP_SLEEP_GESTURE, value ? 1: 0);
-			return true;
+		return true;
 		}
  		return super.onPreferenceTreeClick(preferenceScreen, preference);
     	}
