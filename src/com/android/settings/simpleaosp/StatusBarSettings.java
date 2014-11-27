234dce76bf6d027d8d5c2bc9215cd0b2345f6985
MrBaNkS <banksmi09@gmail.com>
2014-11-26 18:59:27 -0600

    Status bat brightness setting cleanup

--- src/com/android/settings/simpleaosp/StatusBarSettings.java
+++ src/com/android/settings/simpleaosp/StatusBarSettings.java
@@ -1,76 +1,17 @@
-
 package com.android.settings.simpleaosp;
 
-import android.content.ContentResolver;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceScreen;
-import android.preference.SwitchPreference;
-import android.provider.Settings;
-import android.provider.Settings.SettingNotFoundException;
-
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
 
-public class StatusBarSettings extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+public class StatusBarSettings extends SettingsPreferenceFragment {
 
-    // General
+    // Statusbar general category
     private static String STATUS_BAR_GENERAL_CATEGORY = "status_bar_general_category";
-    private static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
-
-    // General
-    private PreferenceCategory mStatusBarGeneralCategory;
-    private SwitchPreference mStatusBarBrightnessControl;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         addPreferencesFromResource(R.xml.status_bar_settings);
-	PreferenceScreen prefSet = getPreferenceScreen();
-
-        // General category
-        mStatusBarGeneralCategory = (PreferenceCategory) findPreference(STATUS_BAR_GENERAL_CATEGORY);
-
- 	// Status bar brightness control
-        mStatusBarBrightnessControl = (SwitchPreference) prefSet.findPreference(STATUS_BAR_BRIGHTNESS_CONTROL);
-        mStatusBarBrightnessControl.setChecked((Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
-                Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL, 0) == 1));
-		mStatusBarBrightnessControl.setOnPreferenceChangeListener(this);
-        try {
-            if (Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
-                    Settings.System.SCREEN_BRIGHTNESS_MODE) == Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC) {
-                mStatusBarBrightnessControl.setEnabled(false);
-                mStatusBarBrightnessControl.setSummary(R.string.status_bar_toggle_info);
-            }
-        } catch (SettingNotFoundException e) {
-        }
-
-        // don't show status bar brightnees control on tablet
-        if (Utils.isTablet(getActivity())) {
-            getPreferenceScreen().removePreference(mStatusBarBrightnessControl);
-        }
     }
-
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-	ContentResolver cr = getActivity().getContentResolver();
-	boolean value = (Boolean) objValue;
-	if (preference == mStatusBarBrightnessControl) {
-            Settings.System.putInt(cr,
-                    Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL, value ? 1 : 0);
-            return true;
-	}
-        return false;
-    }
-
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
- 		return super.onPreferenceTreeClick(preferenceScreen, preference);
-    	}
 }
-
