29e211a98294a000a19aebfd65f04e55d91bcf0d
kmobs <persiansown@gmail.com>
2014-12-07 16:39:54 -0600

    Development Settings: Reenable Nuplayer
    * Nuplayer is now the default in Lollipop, do not uncheck the setting when disabling developer options.
    
    Change-Id: Ic47365192404a866434630a7a4f2f669b8f80233

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -368,6 +368,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         mOpenGLTraces = addListPreference(OPENGL_TRACES_KEY);
         mSimulateColorSpace = addListPreference(SIMULATE_COLOR_SPACE);
         mUseNuplayer = findAndInitSwitchPref(USE_NUPLAYER_KEY);
+        mAllPrefs.add(mUseNuplayer);
         mUSBAudio = findAndInitSwitchPref(USB_AUDIO_KEY);
 
         mWindowAnimationScale = findAndInitAnimationScalePreference(WINDOW_ANIMATION_SCALE_KEY);
@@ -625,6 +626,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         resetDebuggerOptions();
         writeLogdSizeOption(null);
         resetDevelopmentShortcutOptions();
+        resetUseNuplayerOptions();
         writeAnimationScaleOption(0, mWindowAnimationScale, null);
         writeAnimationScaleOption(1, mTransitionAnimationScale, null);
         writeAnimationScaleOption(2, mAnimatorDurationScale, null);
@@ -640,6 +642,10 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         mDontPokeProperties = false;
         pokeSystemProperties();
         mShowUnacAndOvercounted.setChecked(false);
+    }
+
+    private void resetUseNuplayerOptions() {
+        SystemProperties.set(USE_AWESOMEPLAYER_PROPERTY, "0");
     }
 
     private void updateHdcpValues() {
