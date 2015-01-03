31c4abfa02ab3440e46785a7e8a27cb84874df1e
snak3ater <saeedyasir91@gmail.com>
2015-01-03 08:47:15 +0500

    Revert "Setting: PA Clear all button"
    This reverts commit a20f66495dad580fe65f5913d3c83ac528661aba.
    
    Conflicts:
    	res/values/strings.xml
    	res/xml/navigation_bar_settings.xml
    
    Conflicts:
    	res/values/strings.xml

--- src/com/android/settings/simpleaosp/NavigationBarSettings.java
+++ src/com/android/settings/simpleaosp/NavigationBarSettings.java
@@ -1,12 +1,9 @@
 
 package com.android.settings.simpleaosp;
 
-import android.content.ContentResolver;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceScreen;
-import android.preference.SwitchPreference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 
@@ -20,18 +17,14 @@ OnPreferenceChangeListener {
     private static String NAVIGATION_BAR_GENERAL_CATEGORY = "navigation_bar_general_category";
     // Navigation bar height
     private static final String KEY_NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
-    private static final String KEY_CLEAR_ALL_RECENTS_NAVBAR_ENABLED = "clear_all_recents_navbar_enabled";
 
     private ListPreference mNavigationBarHeight;
-    private SwitchPreference mClearAllRecentsNavbar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.navigation_bar_settings);
-	PreferenceScreen prefs = getPreferenceScreen();
-        ContentResolver resolver = getActivity().getContentResolver();
 
         mNavigationBarHeight = (ListPreference) findPreference(KEY_NAVIGATION_BAR_HEIGHT);
         mNavigationBarHeight.setOnPreferenceChangeListener(this);
@@ -40,11 +33,6 @@ OnPreferenceChangeListener {
                 Settings.System.NAVIGATION_BAR_HEIGHT, 48);
         mNavigationBarHeight.setValue(String.valueOf(statusNavigationBarHeight));
         mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntry());
-
-	mClearAllRecentsNavbar = (SwitchPreference) prefs.findPreference(KEY_CLEAR_ALL_RECENTS_NAVBAR_ENABLED);
-        mClearAllRecentsNavbar.setChecked(Settings.System.getInt(resolver,
-                    Settings.System.CLEAR_ALL_RECENTS_NAVBAR_ENABLED, 1) == 1);
-
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
@@ -54,18 +42,6 @@ OnPreferenceChangeListener {
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.NAVIGATION_BAR_HEIGHT, statusNavigationBarHeight);
             mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntries()[index]);
-        }
-        return true;
-    }
-
-   @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mClearAllRecentsNavbar) {
-            Settings.System.putInt(resolver, Settings.System.CLEAR_ALL_RECENTS_NAVBAR_ENABLED,
-                    mClearAllRecentsNavbar.isChecked() ? 1 : 0);
-        } else {
-            return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
         return true;
     }
