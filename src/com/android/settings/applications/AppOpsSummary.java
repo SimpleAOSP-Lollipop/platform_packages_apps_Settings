0e095115b66932daa7d05b5113ce1fd075ffbfe3
MrBaNkS <banksmi09@gmail.com>
2015-01-20 09:47:06 -0600

    settings :remove appops
    Squashed commit of the following:
    
    commit 69ee879b01703c14951362c778aef4b52bd315b7
    Author: MrBaNkS <banksmi09@gmail.com>
    Date:   Tue Jan 20 09:44:46 2015 -0600
    
        Revert "AppOps: Enable App Ops under security setting."
    
        This reverts commit e12ebdffefcf0135f99470f8681c6358061b8e53.
    
    commit cf15a8012581183dabdd5369d888c87f121bc1f2
    Author: MrBaNkS <banksmi09@gmail.com>
    Date:   Tue Jan 20 09:44:29 2015 -0600
    
        Revert "AppOps: Make it better"
    
        This reverts commit 74a98d9dd69b4aa3ac308c30f1c6946e597bd2a9.
    
    commit 49784b5fd026b289b10325e8be048406dcdf266d
    Author: MrBaNkS <banksmi09@gmail.com>
    Date:   Tue Jan 20 09:44:09 2015 -0600
    
        Revert "[3/3] Settings: Add two new AppOps"
    
        This reverts commit 4d274e1e8d28f7317bdb28fc598d32546d6a6e28.

--- src/com/android/settings/applications/AppOpsSummary.java
+++ src/com/android/settings/applications/AppOpsSummary.java
@@ -43,8 +43,7 @@ public class AppOpsSummary extends Fragment {
         AppOpsState.PERSONAL_TEMPLATE,
         AppOpsState.MESSAGING_TEMPLATE,
         AppOpsState.MEDIA_TEMPLATE,
-        AppOpsState.DEVICE_TEMPLATE,
-        AppOpsState.BOOTUP_TEMPLATE
+        AppOpsState.DEVICE_TEMPLATE
     };
 
     int mCurPos;
