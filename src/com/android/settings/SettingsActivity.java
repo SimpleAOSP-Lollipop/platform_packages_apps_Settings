8790f28b2e7a8c4bccbb4787dd4ed63d2e0e993e
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-22 13:26:47 +0000

    QS: add newly added tiles
    * Battery saver
    * Brightness
    * Expanded desktop
    * Screen off
    Change-Id: I64956333bd22d26db6ce72a6149b496d4542bfec

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -68,6 +68,7 @@ import com.android.settings.accessibility.AccessibilitySettings;
 import com.android.settings.accessibility.CaptionPropertiesFragment;
 import com.android.settings.accounts.AccountSettings;
 import com.android.settings.accounts.AccountSyncSettings;
+import com.android.settings.applications.ExpandedDesktopPreferenceFragment;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
@@ -305,6 +306,7 @@ public class SettingsActivity extends Activity
             BlacklistSettings.class.getName(),
 	    DisplayRotation.class.getName(),
 	    QSTiles.class.getName(),
+            ExpandedDesktopPreferenceFragment.class.getName(),
 	    HeadsUpSettings.class.getName()
     };
 
