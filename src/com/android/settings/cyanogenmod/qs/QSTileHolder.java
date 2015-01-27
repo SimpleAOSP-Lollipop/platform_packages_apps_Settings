f5258afc54ec473bbfd4db9f312fd920bb03cf8c
snak3ater <saeedyasir91@gmail.com>
2015-01-27 21:31:49 +0500

    QS: Add Headsup tile [2/2]
    Signed-off-by: snak3ater <saeedyasir91@gmail.com>

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -128,6 +128,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_screen_timeout_on";
                 stringId = R.string.qs_screentimeout_tile;
                 break;
+	    case QSConstants.TILE_HEADS_UP:
+                resourceName = "ic_qs_heads_up_on";
+                stringId = R.string.qs_tile_headsup;
+                break;
             default:
                 return null;
         }
