0cfdc4d3098742b2397b1a16e10ddac5f8cb07e1
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-03 18:02:29 +0000

    Settings: disable quick settings on secure lockscreen feature by default
    Change-Id: I36c9cbe02482c01d76d18ae469038278a58c1d23

--- src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
+++ src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
@@ -58,7 +58,7 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment
         mBlockOnSecureKeyguard = (SwitchPreference) findPreference(PREF_BLOCK_ON_SECURE_KEYGUARD);
         if (lockPatternUtils.isSecure()) {
             mBlockOnSecureKeyguard.setChecked(Settings.Secure.getInt(getContentResolver(),
-                    Settings.Secure.STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD, 1) == 1);
+                    Settings.Secure.STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD, 0) == 1);
             mBlockOnSecureKeyguard.setOnPreferenceChangeListener(this);
         } else if (mBlockOnSecureKeyguard != null) {
             prefSet.removePreference(mBlockOnSecureKeyguard);
