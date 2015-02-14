9a9c4bc79a0553d2e7ccde9bcf475eb7acf28f63
MrBaNkS <banksmi09@gmail.com>
2015-02-14 08:49:21 +0000

    Recents clear all: Remove unneccesary summary code credits:Altaf Mahdi && Euphoria-OS * Taken from commit: https://github.com/Euphoria-OS/android_packages_apps_Settings/commit/339543defea169128b9d6b2b9171fceb8c638a46
    Change-Id: Ifc23282b30d4b725c1279c82d16c2e5c3f6b582a

--- src/com/android/settings/simpleaosp/NavigationBarSettings.java
+++ src/com/android/settings/simpleaosp/NavigationBarSettings.java
@@ -66,8 +66,8 @@ OnPreferenceChangeListener, Indexable {
         int location = Settings.System.getIntForUser(resolver,
                 Settings.System.RECENTS_CLEAR_ALL_LOCATION, 3, UserHandle.USER_CURRENT);
         mRecentsClearAllLocation.setValue(String.valueOf(location));
+        mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
         mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
-        updateRecentsLocation(location);
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
@@ -85,43 +85,13 @@ OnPreferenceChangeListener, Indexable {
             return true;
         } else if (preference == mRecentsClearAllLocation) {
             int location = Integer.valueOf((String) objValue);
+            int index = mRecentsClearAllLocation.findIndexOfValue((String) objValue);
             Settings.System.putIntForUser(getActivity().getContentResolver(),
                     Settings.System.RECENTS_CLEAR_ALL_LOCATION, location, UserHandle.USER_CURRENT);
-            updateRecentsLocation(location);
+            mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntries()[index]);
             return true;
         }
         return false;
-    }
-
-	private void updateRecentsLocation(int value) {
-        ContentResolver resolver = getContentResolver();
-        Resources res = getResources();
-        int summary = -1;
-
-        Settings.System.putInt(resolver, Settings.System.RECENTS_CLEAR_ALL_LOCATION, value);
-
-        if (value == 0) {
-            Settings.System.putInt(resolver, Settings.System.RECENTS_CLEAR_ALL_LOCATION, 0);
-            summary = R.string.recents_clear_all_location_top_right;
-        } else if (value == 1) {
-            Settings.System.putInt(resolver, Settings.System.RECENTS_CLEAR_ALL_LOCATION, 1);
-            summary = R.string.recents_clear_all_location_top_left;
-        } else if (value == 2) {
-            Settings.System.putInt(resolver, Settings.System.RECENTS_CLEAR_ALL_LOCATION, 2);
-            summary = R.string.recents_clear_all_location_top_center;
-        } else if (value == 3) {
-            Settings.System.putInt(resolver, Settings.System.RECENTS_CLEAR_ALL_LOCATION, 3);
-            summary = R.string.recents_clear_all_location_bottom_right;
-        } else if (value == 4) {
-            Settings.System.putInt(resolver, Settings.System.RECENTS_CLEAR_ALL_LOCATION, 4);
-            summary = R.string.recents_clear_all_location_bottom_left;
-        } else if (value == 5) {
-            Settings.System.putInt(resolver, Settings.System.RECENTS_CLEAR_ALL_LOCATION, 5);
-            summary = R.string.recents_clear_all_location_bottom_center;
-        }
-        if (mRecentsClearAllLocation != null && summary != -1) {
-            mRecentsClearAllLocation.setSummary(res.getString(summary));
-        }
     }
 
     public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
